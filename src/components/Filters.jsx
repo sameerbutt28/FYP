73378e4b714c9fc0010eb1f18de1bbff67543035
sameerbutt28 <sameerofficial929@gmail.com>
2024-10-10 01:25:08 +0500

    REMOVING ERRORS

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -68,15 +68,16 @@ const Filters = () => {
     <Form className="bg-base-200 rounded-md px-8 py-4 grid gap-x-4  gap-y-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center">
       {/* SEARCH */}
       <FormInput
-        type="search"
-        label="search product"
-        name="search"
-        size="input-sm"
-        defaultValue="
-      />
+  type="search"
+  label="search product"
+  name="search"
+  size="input-sm"
+  defaultValue=""
+/>
+ 
       {/* CATEGORIES */}
       <FormSelect  
-        label="select category"
+        label= "select category"
         name="category"
         list={selectCategoryList}
         size="select-sm"
